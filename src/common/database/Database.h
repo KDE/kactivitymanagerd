1562b24f5c5a77da75b14e519b9de42a4d237ff2
Ivan Čukić <ivan.cukic@kde.org>
2016-03-08 15:52:01 +0100

    Making it compile with gcc 6.0.0 unreleased version

--- src/common/database/Database.h
+++ src/common/database/Database.h
@@ -1,21 +1,21 @@
 /*
- *   Copyright (C) 2014 - 2016 by Ivan Cukic <ivan.cukic@kde.org>
+ * Copyright 2014 Ivan Cukic <ivan.cukic@kde.org>
  *
- *   This program is free software; you can redistribute it and/or
- *   modify it under the terms of the GNU General Public License as
- *   published by the Free Software Foundation; either version 2 of
- *   the License or (at your option) version 3 or any later version
- *   accepted by the membership of KDE e.V. (or its successor approved
- *   by the membership of KDE e.V.), which shall act as a proxy
- *   defined in Section 14 of version 3 of the license.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
  *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef COMMON_DATABASE_H
@@ -131,11 +131,14 @@ QString parseStarPattern(const QString &pattern, const QString &joker,
     return resultPattern;
 }
 
+inline QString escapeSqliteLikePattern(QString pattern)
+{
+    return pattern.replace(QLatin1String("%"), QLatin1String("\\%")).replace(QLatin1String("_"), QLatin1String("\\_"));
+}
+
 inline QString starPatternToLike(const QString &pattern)
 {
-    return parseStarPattern(pattern, QStringLiteral("%"), [] (QString str) {
-        return str.replace(QLatin1String("%"), QLatin1String("\\%")).replace(QLatin1String("_"), QLatin1String("\\_"));
-    });
+    return parseStarPattern(pattern, QStringLiteral("%"), escapeSqliteLikePattern);
 }
 
 inline QRegExp starPatternToRegex(const QString &pattern)
