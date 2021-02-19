4a1be9322dbb1e8334d70d62e732585847a90371
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-02-19 13:42:00 +0100

    Implement Activities Runner as DBus Runner
    This MR implements the activities runner using the DBus API.
    The runner allows one to view and switch to other activities.
    
    Because the data is already available in the kactivitymanagerd
    process it makes very little sense to have a KRunner plugin which
    fetches this data and produces the results when we can provide this
    functionality in the actual running process.
    
    See https://phabricator.kde.org/T12031 for more discussion
    about the topic in general.

---
+++ src/service/plugins/krunner/ActivityRunner.cpp
@@ -0,0 +1,94 @@
+/*
+ *   SPDX-FileCopyrightText: 2021 Alexander Lohnau <alexander.lohnau@gmx.de>
+ *   SPDX-FileCopyrightText: 2011 Aaron Seigo <aseigo@kde.org>
+ *
+ *   SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include "ActivityRunner.h"
+
+#include <Activities.h>
+#include <KLocalizedString>
+#include <krunner1adaptor.h>
+
+KAMD_EXPORT_PLUGIN(activityrunner, ActivityRunner, "kactivitymanagerd-plugin-activityrunner.json")
+
+ActivityRunner::ActivityRunner(QObject *parent, const QVariantList &args)
+    : Plugin(parent)
+    , m_activitiesService(nullptr)
+    , m_keywordi18n(i18nc("KRunner keyword", "activity"))
+    , m_keyword(QStringLiteral("activity"))
+{
+    Q_UNUSED(args)
+    setName(QStringLiteral("org.kde.ActivityManager.ActivityRunner"));
+    new Krunner1Adaptor(this);
+    qDBusRegisterMetaType<RemoteMatch>();
+    qDBusRegisterMetaType<RemoteMatches>();
+    qDBusRegisterMetaType<RemoteAction>();
+    qDBusRegisterMetaType<RemoteActions>();
+    QDBusConnection::sessionBus().registerObject(QStringLiteral("/runner"), this);
+    QDBusConnection::sessionBus().registerService(QStringLiteral("org.kde.runners.activities"));
+}
+
+ActivityRunner::~ActivityRunner() = default;
+
+bool ActivityRunner::init(QHash<QString, QObject *> &modules)
+{
+    Plugin::init(modules);
+
+    m_activitiesService = modules[QStringLiteral("activities")];
+    return true;
+}
+
+RemoteMatches ActivityRunner::Match(const QString &query)
+{
+    Q_ASSERT(m_activitiesService);
+    const auto currentActivity = Plugin::retrieve<QString>(m_activitiesService, "CurrentActivity", "QString");
+    auto activities = Plugin::retrieve<ActivityInfoList>(m_activitiesService, "ListActivitiesWithInformation", "ActivityInfoList");
+
+    const QString term = query.trimmed();
+    bool list = false;
+    QString name;
+
+    if (term == m_keyword || term == m_keywordi18n) {
+        list = true;
+    } else if (term.startsWith(m_keyword, Qt::CaseInsensitive)) {
+        name = term.right(term.size() - m_keyword.size()).trimmed();
+    } else if (term.startsWith(m_keywordi18n, Qt::CaseInsensitive)) {
+        name = term.right(term.size() - m_keywordi18n.size()).trimmed();
+    } else {
+        return {};
+    }
+
+    QList<RemoteMatch> matches;
+    for (const ActivityInfo &activityInfo : qAsConst(activities)) {
+        if (currentActivity != activityInfo.id) {
+            auto info = Plugin::retrieve<ActivityInfo>(m_activitiesService, "ActivityInformation", "ActivityInfo", Q_ARG(QString, activityInfo.id));
+            if (list || info.name.startsWith(name, Qt::CaseInsensitive)) {
+                RemoteMatch m;
+                m.id = activityInfo.id;
+                m.text = i18n("Switch to \"%1\"", info.name);
+                m.iconName = info.icon;
+                m.type = Type::ExactMatch;
+                m.relevance = 0.7 + ((activityInfo.state == Activities::State::Running || activityInfo.state == Activities::State::Starting) ? 0.1 : 0);
+                matches.append(m);
+            }
+        }
+    }
+
+    return matches;
+}
+
+RemoteActions ActivityRunner::Actions()
+{
+    return {};
+}
+
+void ActivityRunner::Run(const QString &matchId, const QString &actionId)
+{
+    Q_UNUSED(actionId)
+    QMetaObject::invokeMethod(m_activitiesService, "SetCurrentActivity", Q_ARG(QString, matchId));
+}
+
+#include "ActivityRunner.moc"
+
---
+++ src/service/plugins/krunner/ActivityRunner.h
@@ -0,0 +1,31 @@
+/*
+ *   SPDX-FileCopyrightText: 2021 Alexander Lohnau <alexander.lohnau@gmx.de>
+ *
+ *   SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef PLUGINS_RUN_APPLICATION_PLUGIN_H
+#define PLUGINS_RUN_APPLICATION_PLUGIN_H
+
+#include <Plugin.h>
+#include "dbusutils.h"
+
+class ActivityRunner : public Plugin {
+    Q_OBJECT
+
+public:
+    explicit ActivityRunner(QObject *parent = nullptr, const QVariantList &args = QVariantList());
+    ~ActivityRunner() override;
+    bool init(QHash<QString, QObject *> &modules) override;
+
+    void Run(const QString &matchId, const QString &actionId);
+    RemoteMatches Match(const QString &query);
+    RemoteActions Actions();
+
+private:
+    QObject *m_activitiesService;
+    const QString m_keywordi18n;
+    const QString m_keyword;
+};
+
+#endif // PLUGINS_RUN_APPLICATION_PLUGIN_H
