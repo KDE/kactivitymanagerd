3ef46c5b68cf73eac4da87ed5322f1ea3c207676
David Edmundson <kde@davidedmundson.co.uk>
2023-10-17 12:06:49 +0100

    Remove dead code
    This belongs purely in library code

--- src/common/dbus/common.h
+++ src/common/dbus/common.h
@@ -21,25 +21,4 @@
 #define KAMD_DBUS_OBJECT(A)                                                    \
     QLatin1String("org.kde.ActivityManager." A)
 
-#define KAMD_DBUS_INTERFACE(OBJECT_PATH, OBJECT, PARENT)                       \
-    QDBusInterface(KAMD_DBUS_SERVICE,                                          \
-                   KAMD_DBUS_OBJECT_PATH(OBJECT_PATH),                         \
-                   KAMD_DBUS_OBJECT(OBJECT),                                   \
-                   QDBusConnection::sessionBus(),                              \
-                   PARENT)
-
-#define KAMD_DBUS_DECL_INTERFACE(VAR, OBJECT_PATH, OBJECT)                     \
-    QDBusInterface VAR(KAMD_DBUS_SERVICE,                                      \
-                   KAMD_DBUS_OBJECT_PATH(OBJECT_PATH),                         \
-                   KAMD_DBUS_OBJECT(OBJECT),                                   \
-                   QDBusConnection::sessionBus(),                              \
-                   nullptr)
-
-#define KAMD_DBUS_CLASS_INTERFACE(OBJECT_PATH, OBJECT, PARENT)                 \
-    org::kde::ActivityManager::OBJECT(                                         \
-                KAMD_DBUS_SERVICE,                                             \
-                KAMD_DBUS_OBJECT_PATH(OBJECT_PATH),                            \
-                QDBusConnection::sessionBus(),                                 \
-                PARENT)
-
 
