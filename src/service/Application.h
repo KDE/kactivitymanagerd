KDE/kactivitymanagerd

commit fb2c4167c782ef62b7f5f54a2e0d94bf9593ec87

Fix most of clazy warnings, remove dead code

src/service/Application.h
@@ -39,11 +39,12 @@ public:
     Activities &activities() const;
     Features &features() const;
 
+    QString serviceVersion() const;
+    QStringList loadedPlugins() const;
+
 public Q_SLOTS:
     void quit();
-    QString serviceVersion() const;
     bool loadPlugin(const QString &plugin);
-    QStringList loadedPlugins() const;
 
 private Q_SLOTS:
     void init();

src/service/Activities.cpp
@@ -68,7 +68,8 @@ Activities::Private::Private(Activities *parent)
     // Do we have a running activity?
     bool atLeastOneRunning = false;
 
-    for (const auto &activity : activityNameConfig().keyList()) {
+    const auto activtiesList = activityNameConfig().keyList();
+    for (const auto &activity : activtiesList) {
         auto state = runningActivities.contains(activity) ? Activities::Running : stoppedActivities.contains(activity) ? Activities::Stopped : defaultState;
 
         activities[activity] = state;
@@ -122,7 +123,7 @@ void Activities::Private::loadLastActivity()
     // If there are no public activities, try to load the last used activity
     const auto lastUsedActivity = mainConfig().readEntry("currentActivity", QString());
 
-    setCurrentActivity((lastUsedActivity.isEmpty() && activities.size() > 0) ? activities.keys().at(0) : lastUsedActivity);
+    setCurrentActivity((lastUsedActivity.isEmpty() && activities.size() > 0) ? activities.constBegin().key() : lastUsedActivity);
 }
 
 Activities::Private::~Private()
@@ -493,7 +494,7 @@ QStringList Activities::ListActivities() const
     QReadLocker lock(&d->activitiesLock);
 
     QStringList s;
-    for (const auto &a : d->sortedActivities) {
+    for (const auto &a : std::as_const(d->sortedActivities)) {
         s << a.id;
     }
     return s;
@@ -504,7 +505,7 @@ QStringList Activities::ListActivities(int state) const
     QReadLocker lock(&d->activitiesLock);
 
     QStringList s;
-    for (const auto &a : d->sortedActivities) {
+    for (const auto &a : std::as_const(d->sortedActivities)) {
         if (a.state == (State)state) {
             s << a.id;
         }

src/service/Activities.h
@@ -50,11 +50,57 @@ public:
     ~Activities() override;
 
     // workspace activities control
-public Q_SLOTS:
     /**
      * @returns the id of the current activity, empty string if none
      */
     QString CurrentActivity() const;
+
+    /**
+     * @returns the state of the activity
+     * @param activity id of the activity
+     */
+    int ActivityState(const QString &activity) const;
+
+    /**
+     * @returns the list of all existing activities
+     */
+    QStringList ListActivities() const;
+
+    /**
+     * @returns the list of activities with the specified state
+     * @param state state
+     */
+    QStringList ListActivities(int state) const;
+
+    /**
+     * @returns the name of the specified activity
+     * @param activity id of the activity
+     */
+    QString ActivityName(const QString &activity) const;
+
+    /**
+     * @returns the description of the specified activity
+     * @param activity id of the activity
+     */
+    QString ActivityDescription(const QString &activity) const;
+
+    /**
+     * @returns the icon of the specified activity
+     * @param activity id of the activity
+     */
+    QString ActivityIcon(const QString &activity) const;
+
+    /**
+     * @returns a list of activities with basic info about them
+     */
+    ActivityInfoList ListActivitiesWithInformation() const;
+
+    /**
+     * @returns the info about an activity
+     */
+    ActivityInfo ActivityInformation(const QString &activity) const;
+
+public Q_SLOTS:
 
     /**
      * Sets the current activity
@@ -92,33 +138,10 @@ public Q_SLOTS:
     void StopActivity(const QString &activity);
 
     /**
-     * @returns the state of the activity
-     * @param activity id of the activity
-     */
-    int ActivityState(const QString &activity) const;
-
-    /**
      * Removes the specified activity
      * @param activity id of the activity to delete
      */
     void RemoveActivity(const QString &activity);
-
-    /**
-     * @returns the list of all existing activities
-     */
-    QStringList ListActivities() const;
-
-    /**
-     * @returns the list of activities with the specified state
-     * @param state state
-     */
-    QStringList ListActivities(int state) const;
-
-    /**
-     * @returns the name of the specified activity
-     * @param activity id of the activity
-     */
-    QString ActivityName(const QString &activity) const;
 
     /**
      * Sets the name of the specified activity
@@ -128,12 +151,6 @@ public Q_SLOTS:
     void SetActivityName(const QString &activity, const QString &name);
 
     /**
-     * @returns the description of the specified activity
-     * @param activity id of the activity
-     */
-    QString ActivityDescription(const QString &activity) const;
-
-    /**
      * Sets the description of the specified activity
      * @param activity id of the activity
      * @param description description to be set
@@ -141,28 +158,11 @@ public Q_SLOTS:
     void SetActivityDescription(const QString &activity, const QString &description);
 
     /**
-     * @returns the icon of the specified activity
-     * @param activity id of the activity
-     */
-    QString ActivityIcon(const QString &activity) const;
-
-    /**
      * Sets the icon of the specified activity
      * @param activity id of the activity
      * @param icon icon to be set
      */
     void SetActivityIcon(const QString &activity, const QString &icon);
-
-public Q_SLOTS:
-    /**
-     * @returns a list of activities with basic info about them
-     */
-    ActivityInfoList ListActivitiesWithInformation() const;
-
-    /**
-     * @returns the info about an activity
-     */
-    ActivityInfo ActivityInformation(const QString &activity) const;
 
 Q_SIGNALS:
     /**

src/service/Application.cpp
@@ -193,7 +193,7 @@ Application::~Application()
     qCDebug(KAMD_LOG_APPLICATION) << "Cleaning up...";
 
     // Waiting for the threads to finish
-    for (const auto thread : s_moduleThreads) {
+    for (const auto thread : std::as_const(s_moduleThreads)) {
         thread->quit();
         thread->wait();
 
@@ -201,7 +201,7 @@ Application::~Application()
     }
 
     // Deleting plugin objects
-    for (const auto plugin : d->plugins) {
+    for (const auto plugin : std::as_const(d->plugins)) {
         delete plugin;
     }
 

src/service/Features.h
@@ -29,15 +29,16 @@ public:
     explicit Features(QObject *parent = nullptr);
     ~Features() override;
 
-public Q_SLOTS:
+    QStringList ListFeatures(const QString &module) const;
+
+    QDBusVariant GetValue(const QString &property) const;
+
     /**
      * Is the feature backend available?
      */
     bool IsFeatureOperational(const QString &feature) const;
 
-    QStringList ListFeatures(const QString &module) const;
-
-    QDBusVariant GetValue(const QString &property) const;
+public Q_SLOTS:
 
     void SetValue(const QString &property, const QDBusVariant &value);
 

src/service/ksmserver/KSMServer.cpp
@@ -54,7 +54,7 @@ void KSMServer::stopActivitySession(const QString &activity)
 void KSMServer::Private::processLater(const QString &activity, bool start)
 {
     if (kwin->isValid()) {
-        for (const auto &item : queue) {
+        for (const auto &item : std::as_const(queue)) {
             if (item.first == activity) {
                 return;
             }

src/service/ksmserver/KSMServer_p.h
@@ -12,9 +12,6 @@
 // Qt
 #include <QPair>
 
-// STD
-#include <memory>
-
 class QDBusServiceWatcher;
 class QDBusInterface;
 class QDBusPendingCallWatcher;

src/service/plugins/activitytemplates/TemplatesPlugin.cpp
@@ -11,9 +11,6 @@
 #include <QDBusConnection>
 #include <QStringList>
 
-// Utils
-#include <utils/for_each_assoc.h>
-
 // Local
 #include "templatesadaptor.h"
 
@@ -82,8 +79,6 @@ void TemplatesPlugin::setFeatureValue(const QStringList &property, const QDBusVa
 
 void TemplatesPlugin::createActivity(const QDBusVariant &_values)
 {
-    using namespace kamd::utils;
-
     QVariantHash values = _values.variant().toHash();
 
     auto takeStringValue = [&values](const QString &key) {
@@ -103,9 +98,9 @@ void TemplatesPlugin::createActivity(const QDBusVariant &_values)
     KConfigGroup pluginConfig(config());
     KConfigGroup activityConfig(&pluginConfig, id);
 
-    for_each_assoc(values, [&activityConfig](const QString &key, const QVariant &value) {
+    for (const auto &[key, value] : values.asKeyValueRange()) {
         activityConfig.writeEntry(key, value);
-    });
+    }
 
     activityConfig.sync();
 

commit 9ac3827ce02d3e2940083e958b77424c4c25e601

service/Application: make serviceVersion a slot again
Fixes a regression caused by fb2c4167c782ef62b7f5f54a2e0d94bf9593ec87

BUG: 504721

src/service/Application.h
@@ -39,12 +39,12 @@ public:
     Activities &activities() const;
     Features &features() const;
 
-    QString serviceVersion() const;
     QStringList loadedPlugins() const;
 
 public Q_SLOTS:
     void quit();
     bool loadPlugin(const QString &plugin);
+    QString serviceVersion() const;
 
 private Q_SLOTS:
     void init();