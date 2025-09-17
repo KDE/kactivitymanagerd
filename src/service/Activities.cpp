KDE/kactivitymanagerd

commit fc6ce6164d5af7f51ff2d98bccaaf984023ae758

Drop more activities state code
Amends 88eedeb6378fef547e4589bb0b76bbd9c53a0063

src/service/Activities.cpp
@@ -49,11 +49,6 @@ Activities::Private::Private(Activities *parent)
     //     << QStandardPaths::standardLocations(config.locationType())
     //     ;
 
-    const auto activtiesList = activityNameConfig().keyList();
-    for (const auto &activity : activtiesList) {
-        activities[activity] = Activities::Running;
-    }
-
     // Is this our first start?
     if (activities.isEmpty()) {
         // We need to add this only after the service has been properly started
@@ -74,8 +69,8 @@ Activities::Private::Private(Activities *parent)
 void Activities::Private::updateSortedActivityList()
 {
     QList<ActivityInfo> a;
-    for (auto activityKey = activities.keyBegin(); activityKey != activities.keyEnd(); ++activityKey) {
-        a.append(q->ActivityInformation(*activityKey));
+    for (auto activity : std::as_const(activities)) {
+        a.append(q->ActivityInformation(activity));
     }
 
     std::sort(a.begin(), a.end(), &nameBasedOrdering);
@@ -91,7 +86,7 @@ void Activities::Private::loadLastActivity()
     // If there are no public activities, try to load the last used activity
     const auto lastUsedActivity = mainConfig().readEntry("currentActivity", QString());
 
-    setCurrentActivity((lastUsedActivity.isEmpty() && activities.size() > 0) ? activities.constBegin().key() : lastUsedActivity);
+    setCurrentActivity((lastUsedActivity.isEmpty() && activities.size() > 0) ? *activities.constBegin() : lastUsedActivity);
 }
 
 Activities::Private::~Private()
@@ -138,7 +133,7 @@ bool Activities::Private::setCurrentActivity(const QString &activity)
 
 bool Activities::Private::previousActivity()
 {
-    const auto a = q->ListActivities(Activities::Running);
+    const auto a = q->ListActivities();
 
     for (int i = 0; i < a.count(); ++i) {
         if (a[i] == currentActivity) {
@@ -151,7 +146,7 @@ bool Activities::Private::previousActivity()
 
 bool Activities::Private::nextActivity()
 {
-    const auto a = q->ListActivities(Activities::Running);
+    const auto a = q->ListActivities();
 
     for (int i = 0; i < a.count(); ++i) {
         if (a[i] == currentActivity) {
@@ -184,11 +179,9 @@ QString Activities::Private::addActivity(const QString &name)
 
         // Saves the activity info to the config
 
-        activities[activity] = Invalid;
+        activities << activity;
         activitiesCount = activities.size();
     }
-
-    setActivityState(activity, Running);
 
     q->SetActivityName(activity, name);
 
@@ -270,25 +263,6 @@ void Activities::Private::configSync()
     // Stop the timer and reset the interval to zero
     QMetaObject::invokeMethod(&configSyncTimer, "stop", Qt::QueuedConnection);
     config.sync();
-}
-
-void Activities::Private::setActivityState(const QString &activity, Activities::State state)
-{
-    {
-        QWriteLocker lock(&activitiesLock);
-
-        Q_ASSERT(activities.contains(activity));
-
-        if (activities.value(activity) == state) {
-            return;
-        }
-
-        activities[activity] = state;
-    }
-
-    Q_EMIT q->ActivityStateChanged(activity, state);
-
-    updateSortedActivityList();
 }
 
 // Main
@@ -378,19 +352,6 @@ QStringList Activities::ListActivities() const
     return s;
 }
 
-QStringList Activities::ListActivities(int state) const
-{
-    QReadLocker lock(&d->activitiesLock);
-
-    QStringList s;
-    for (const auto &a : std::as_const(d->sortedActivities)) {
-        if (a.state == (State)state) {
-            s << a.id;
-        }
-    }
-    return s;
-}
-
 QList<ActivityInfo> Activities::ListActivitiesWithInformation() const
 {
     using namespace kamd::utils;
@@ -402,7 +363,7 @@ QList<ActivityInfo> Activities::ListActivitiesWithInformation() const
 
 ActivityInfo Activities::ActivityInformation(const QString &activity) const
 {
-    return ActivityInfo{activity, ActivityName(activity), ActivityDescription(activity), ActivityIcon(activity), ActivityState(activity)};
+    return ActivityInfo{activity, ActivityName(activity), ActivityDescription(activity), ActivityIcon(activity)};
 }
 
 #define CREATE_GETTER_AND_SETTER(What)                                                                                                                         \
@@ -433,14 +394,6 @@ CREATE_GETTER_AND_SETTER(Description)
 CREATE_GETTER_AND_SETTER(Icon)
 
 #undef CREATE_GETTER_AND_SETTER
-
-// Main
-
-int Activities::ActivityState(const QString &activity) const
-{
-    QReadLocker lock(&d->activitiesLock);
-    return d->activities.contains(activity) ? d->activities[activity] : Invalid;
-}
 
 #include "moc_Activities.cpp"
 

src/common/dbus/org.kde.ActivityManager.Activities.cpp
@@ -34,7 +34,7 @@ QDBusArgument &operator<<(QDBusArgument &arg, const ActivityInfo &r)
     arg << r.name;
     arg << r.description;
     arg << r.icon;
-    arg << r.state;
+    arg << 2; // unused
 
     arg.endStructure();
 
@@ -45,11 +45,13 @@ const QDBusArgument &operator>>(const QDBusArgument &arg, ActivityInfo &r)
 {
     arg.beginStructure();
 
+    int unused;
+
     arg >> r.id;
     arg >> r.name;
     arg >> r.description;
     arg >> r.icon;
-    arg >> r.state;
+    arg >> unused;
 
     arg.endStructure();
 

src/common/dbus/org.kde.ActivityManager.Activities.h
@@ -15,18 +15,12 @@ struct ActivityInfo {
     QString name;
     QString description;
     QString icon;
-    int state;
 
-    ActivityInfo(const QString &id = QString(),
-                 const QString &name = QString(),
-                 const QString &description = QString(),
-                 const QString &icon = QString(),
-                 int state = 0)
+    ActivityInfo(const QString &id = QString(), const QString &name = QString(), const QString &description = QString(), const QString &icon = QString())
         : id(id)
         , name(name)
         , description(description)
         , icon(icon)
-        , state(state)
     {
     }
 };

src/common/dbus/org.kde.ActivityManager.Activities.xml
@@ -20,20 +20,12 @@
       <arg type="s" direction="out"/>
       <arg name="name" type="s" direction="in"/>
     </method>
-    <method name="ActivityState">
-      <arg type="i" direction="out"/>
-      <arg name="activity" type="s" direction="in"/>
-    </method>
     <method name="RemoveActivity">
       <arg name="activity" type="s" direction="in"/>
     </method>
 
     <method name="ListActivities">
       <arg type="as" direction="out"/>
-    </method>
-    <method name="ListActivities">
-      <arg type="as" direction="out"/>
-      <arg name="state" type="i" direction="in"/>
     </method>
 
     <method name="ListActivitiesWithInformation">
@@ -105,10 +97,6 @@
     <signal name="ActivityIconChanged">
       <arg name="activity" type="s" direction="out"/>
       <arg name="icon" type="s" direction="out"/>
-    </signal>
-    <signal name="ActivityStateChanged">
-      <arg name="activity" type="s" direction="out"/>
-      <arg name="state" type="i" direction="out"/>
     </signal>
 
   </interface>

src/service/Activities.h
@@ -28,16 +28,6 @@ class Activities : public Module
 
 public:
     /**
-     * Activity state
-     * @note: Do not change the values, needed for bit-operations
-     */
-    enum State {
-        Invalid = 0,
-        Running = 2,
-        Starting = 3,
-    };
-
-    /**
      * Creates new Activities object
      */
     explicit Activities(QObject *parent = nullptr);
@@ -55,21 +45,9 @@ public Q_SLOTS:
     QString CurrentActivity() const;
 
     /**
-     * @returns the state of the activity
-     * @param activity id of the activity
-     */
-    int ActivityState(const QString &activity) const;
-
-    /**
      * @returns the list of all existing activities
      */
     QStringList ListActivities() const;
-
-    /**
-     * @returns the list of activities with the specified state
-     * @param state state
-     */
-    QStringList ListActivities(int state) const;
 
     /**
      * @returns the name of the specified activity
@@ -195,11 +173,6 @@ Q_SIGNALS:
      * @param activity id of the changed activity
      */
     void ActivityChanged(const QString &activity);
-
-    /**
-     * Emitted when the state of activity is changed
-     */
-    void ActivityStateChanged(const QString &activity, int state);
 
 private:
     D_PTR;

src/service/Activities_p.h
@@ -38,15 +38,13 @@ public Q_SLOTS:
     void updateSortedActivityList();
 
 public:
-    void setActivityState(const QString &activity, Activities::State state);
-
     QTimer configSyncTimer;
     KConfig config;
 
     // Interface to the session management
     KSMServer *ksmserver;
 
-    QHash<QString, Activities::State> activities;
+    QSet<QString> activities;
     QList<ActivityInfo> sortedActivities;
     QReadWriteLock activitiesLock;
     QString currentActivity;

src/service/plugins/krunner/ActivityRunner.cpp
@@ -67,7 +67,7 @@ RemoteMatches ActivityRunner::Match(const QString &query)
                 m.text = i18n("Switch to \"%1\"", info.name);
                 m.iconName = info.icon;
                 m.type = Type::ExactMatch;
-                m.relevance = 0.7 + ((activityInfo.state == Activities::State::Running || activityInfo.state == Activities::State::Starting) ? 0.1 : 0);
+                m.relevance = 0.7;
                 matches.append(m);
             }
         }

src/service/plugins/runapplication/RunApplicationPlugin.cpp
@@ -16,14 +16,6 @@
 
 #include <KService>
 
-namespace
-{
-enum ActivityState {
-    Running = 2,
-    Stopped = 4,
-};
-}
-
 K_PLUGIN_CLASS(RunApplicationPlugin)
 
 RunApplicationPlugin::RunApplicationPlugin(QObject *parent)
@@ -44,7 +36,6 @@ bool RunApplicationPlugin::init(QHash<QString, QObject *> &modules)
     m_activitiesService = modules[QStringLiteral("activities")];
 
     connect(m_activitiesService, SIGNAL(CurrentActivityChanged(QString)), this, SLOT(currentActivityChanged(QString)));
-    connect(m_activitiesService, SIGNAL(ActivityStateChanged(QString, int)), this, SLOT(activityStateChanged(QString, int)));
 
     const auto currentActivity = Plugin::retrieve<QString>(m_activitiesService, "CurrentActivity");
 
@@ -74,24 +65,8 @@ void RunApplicationPlugin::currentActivityChanged(const QString &activity)
     executeIn(activityDirectory(activity) + QStringLiteral("activated"));
 
     if (!m_previousActivities.contains(activity)) {
-        // This is the first time we have switched
-        // to this activity in the current session,
-        // pretending it has just been started
-        activityStateChanged(activity, Running);
-
         m_previousActivities << activity;
     }
-}
-
-void RunApplicationPlugin::activityStateChanged(const QString &activity, int state)
-{
-    auto directory = (state == Running) ? QStringLiteral("started") : (state == Stopped) ? QStringLiteral("stopped") : QString();
-
-    if (directory.isEmpty()) {
-        return;
-    }
-
-    executeIn(activityDirectory(activity) + directory);
 }
 
 void RunApplicationPlugin::executeIn(const QString &path) const

commit 7fa75bdc3f7c895df0943e697d3c94fba67d7367

Activities: Add back activity loading at start
This loading part was accidentally deleted in commit
fc6ce6164d5af7f51ff2d98bccaaf984023ae758
which caused the activities list never load anything
at class initialization, ending up with always
empty activities list

src/service/Activities.cpp
@@ -48,6 +48,10 @@ Activities::Private::Private(Activities *parent)
     //     << config.locationType()
     //     << QStandardPaths::standardLocations(config.locationType())
     //     ;
+    const auto activityList = activityNameConfig().keyList();
+    for (const auto &activity : activityList) {
+        activities.insert(activity);
+    }
 
     // Is this our first start?
     if (activities.isEmpty()) {